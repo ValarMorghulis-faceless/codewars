bcc6071309737e2d62768c777d1feb0a0655cd52
Giorgi samkhar <padregiorgi777@gmail.com>
2022-09-01 01:30:04 +0400

    Upgraded factorialdecomp

--- 5kyu/Factorial-decomposition.swift
+++ 5kyu/Factorial-decomposition.swift
@@ -3,14 +3,14 @@ import Foundation
 func decomp(_ m: Int) -> String {
     // your code
   
-  let primenumbers = [2,3,5,7,11,13,17,19,23,29]
+  let primenumbers = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
 
 
 var b = m
 
   var fact = 1
   for _ in 0..<b{
-    fact *= m
+    fact *= b
     b -= 1
   }
 
@@ -20,17 +20,33 @@ var r = 0
 var t = ""
 
 var h = 0
-
-while h < primenumbers.count{
-    if fact % primenumbers[h] == 0{
-        fact /= primenumbers[h]
-        r += 1
-    }else{
-        t += "\(primenumbers[h])^\(r) * "
-        h += 1
-        r = 0
+    
+    while h < primenumbers.count {
+        if fact % primenumbers[h] == 0 {
+            fact /= primenumbers[h]
+            r += 1
+        }else {
+            if r == 0 {
+                break
+            }else if r > 1 {
+            t += "\(primenumbers[h])^\(r) * "
+            h += 1
+            r = 0
+            }else {
+                t += "\(primenumbers[h]) * "
+                h += 1
+                r = 0
+            }
+        }
+        if fact == 0{
+            break
+        }
+        
     }
-}
-return t
+    
+    t.removeLast()
+    t.removeLast()
+    t.removeLast()
+    return t
   
 }
