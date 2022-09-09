5d670f673e3a134d14111513aa844cce4070fa52
Giorgi samkhar <padregiorgi777@gmail.com>
2022-09-10 02:55:42 +0400

    Disemvowel Trolls

---
+++ 7kyu/DisemvowelTrolls.swift
@@ -0,0 +1,8 @@
+func disemvowel(_ s: String) -> String {
+  var phrase = s
+let vowels: Set<Character> = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U" ]
+phrase.removeAll(where: { vowels.contains($0) })
+  
+  
+  return phrase
+}
