246f2e438a62dcda34999de522043618b0cbdfe7
Giorgi samkhar <padregiorgi777@gmail.com>
2022-09-30 00:11:24 +0400

    Common Denominators fixed

--- 5kyu/CommonDenominator.swift
+++ 5kyu/CommonDenominator.swift
@@ -13,23 +13,27 @@ if l.count < 3 {
 func lcm(_ m: Int, _ n: Int) -> Int {
   return m*n / gcd(m, n)
 }
-var i = 0
-var num = 0
-while true {
-    num = (lcm(l[i].1, l[i + 1].1))
-    num = lcm(num, l[i + 2].1)
-    i += 1
-    if i > l.count - 3 {
-        break
-    }
-}
-
+    var rez1 = l.map {$0.0}.reduce(1){lcm($0, $1)}
+    let rez2 = l.map {$0.1}.reduce(1){lcm($0, $1)}
+    rez1 = lcm(rez1, rez2)
+    print(rez1)
+    
 let arr2 = l.map { (num1,num2) -> (Int,Int) in
-    var number = num1
-    number *=  num / num2
-    return (number,num)
+    var number: Double = Double(num1)
+    number *=  Double(rez1) / Double(num2)
+    return (Int(number),rez1)
 }
+   
+    var result = arr2.map { $0.0 }.reduce(0) {gcd($0, $1)}
+     result = gcd(result, arr2[0].1)
+  
 
-return arr2
-
+    let arr3 = arr2.map { (num1,num2) -> (Int,Int) in
+        var number = Double(num1)
+        var number2 = Double(num2)
+        number /= Double(result)
+        number2 /= Double(result)
+        return (Int(number),Int(number2))
+    }
+return arr3
 }
